9c9f005e501ed5c7fd35c8e914e08acb8b46a799
emarkees <emarkees@gmail.com>
2024-05-23 12:27:11 -0700

    Add modal files

---
+++ src/Modal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Modal.css'
+
+const Modal = ({ children, onClose }) => {
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains('modal__overlay')) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="modal__overlay" onClick={handleOverlayClick}>
+      <div className="modal__content">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
