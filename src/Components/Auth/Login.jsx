9900e2eacb1ca9e8d2ee8e399c90ec9be15f50ba
emarkees <emarkees@gmail.com>
2024-05-23 14:47:00 -0700

    Add login route to App.js

---
+++ src/Components/Auth/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './Login.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Login = ({ onClose }) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    // Implement your sign-in logic here
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user);
+        navigate('/dashboard');
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="modal__overlay">
+      <div className="login__container__head">
+        {/* <img
+            className="login__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+            alt="Amazon logo"
+          /> */}
+
+        <div className="login__container">
+          <h1>Welcome to NExGym</h1>
+          <form onSubmit={handleSignIn}>
+            <i
+              className="fa-solid fa-xmark fa-2x close__button"
+              onClick={onClose}
+            />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="btn-primary login__signInButton"
+              onClick={signIn}
+            >
+
+              Sign In
+            </button>
+          </form>
+          <p>Terms and Conditions</p>
+          <Link to="/signUp">
+            <span className="login__registerButton">Create Your Account</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
