82bb0cc9994c5651c3c5dafea830c9a4c7e45520
emarkees <emarkees@gmail.com>
2024-05-23 12:25:30 -0700

    Add Header.css files

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -48,29 +48,41 @@ function Header() {
 
       <div className="header__basket__nav">
         <div className="header__optionBasket">
-          <i className="fa-solid fa-cart-shopping"></i>
+          <i className="fa-solid fa-cart-shopping" />
           <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
         <div className="header__nav">
           <div className="burger" onClick={toggleDropdown}>
-            <div className={`burger__line ${isDropdownOpen ? "active" : ""}`}></div>
-            <div className={`burger__line ${isDropdownOpen ? "active" : ""}`}></div>
-            <div className={`burger__line ${isDropdownOpen ? "active" : ""}`}></div>
+            <div className={`burger__line ${isDropdownOpen ? "active" : ""}`} />
+            <div className={`burger__line ${isDropdownOpen ? "active" : ""}`} />
+            <div className={`burger__line ${isDropdownOpen ? "active" : ""}`} />
           </div>
           <div className={`dropdown__menu ${isDropdownOpen ? "show" : ""}`}>
             <ul>
               <li>
-                <RouterLink to="/about" className="dropdown__item" onClick={handleLinkClick}>
+                <RouterLink
+                  to="/about"
+                  className="dropdown__item"
+                  onClick={handleLinkClick}
+                >
                   About Us
                 </RouterLink>
               </li>
               <li>
-                <RouterLink to="/schedule" className="dropdown__item" onClick={handleLinkClick}>
+                <RouterLink
+                  to="/schedule"
+                  className="dropdown__item"
+                  onClick={handleLinkClick}
+                >
                   Schedule
                 </RouterLink>
               </li>
               <li>
-                <RouterLink to="/contact" className="dropdown__item" onClick={handleLinkClick}>
+                <RouterLink
+                  to="/contact"
+                  className="dropdown__item"
+                  onClick={handleLinkClick}
+                >
                   Contact
                 </RouterLink>
               </li>
@@ -78,15 +90,19 @@ function Header() {
                 <ScrollLink
                   to="visit"
                   className="dropdown__item"
-                  smooth={true}
+                  smooth
                   duration={500}
                   onClick={handleLinkClick}
                 >
-                 Visit
+                  Visit
                 </ScrollLink>
               </li>
               <li>
-                <RouterLink to="/store" className="dropdown__item" onClick={handleLinkClick}>
+                <RouterLink
+                  to="/store"
+                  className="dropdown__item"
+                  onClick={handleLinkClick}
+                >
                   Store
                 </RouterLink>
               </li>
@@ -102,9 +118,9 @@ function Header() {
               </li>
             </ul>
             <div className="social__page">
-              <i className="fa-brands fa-2x fa-instagram"></i>
-              <i className="fa-brands fa-2x fa-youtube"></i>
-              <i className="fa-brands fa-2x fa-square-facebook"></i>
+              <i className="fa-brands fa-2x fa-instagram" />
+              <i className="fa-brands fa-2x fa-youtube" />
+              <i className="fa-brands fa-2x fa-square-facebook" />
             </div>
           </div>
         </div>
@@ -116,4 +132,3 @@ function Header() {
 }
 
 export default Header;
-
